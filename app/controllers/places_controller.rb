ef1d2d3fa121903dd54cbde22c1e0ae77f84caf9
Angeles Guyot <angeles.guyot@kellogg.northwestern.edu>
2024-05-29 23:35:14 +0000

    aa

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,10 +3,10 @@ class PlacesController < ApplicationController
   def index
     @places = Place.all
   end
-
+  
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] }, "user_id"=> current_user["id"])
+    @entries = Entry.where({ "place_id" => @place["id"], "user_id" => session["user_id"] })
   end
 
   def new
